00c38378bc093bc6cc7b2adec3362cc090131aa1
tmsperera <tmsperera@gmail.com>
2025-03-21 18:41:49 +0530

    Test Workflows

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -10,6 +10,6 @@ class ExampleTest extends TestCase
      * A basic test example.
      */
     public function test_that_true_is_true(): void{
-        $this->assertTrue(true);
+            $this->assertTrue(true);
     }
 }
