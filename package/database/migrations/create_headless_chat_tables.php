39b63fabdd58beb0073a8d29ee404b9ccb39274f
tmsperera <tmsperera@gmail.com>
2025-03-22 23:31:51 +0530

    Revert "Updated migrations"
    This reverts commit ff8b46fe2289ee8cd07d736854a3420012295b73.

--- package/database/migrations/create_headless_chat_tables.php
+++ package/database/migrations/create_headless_chat_tables.php
@@ -3,18 +3,12 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use TMSPerera\HeadlessChat\Config\HeadlessChatConfig;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        $conversation = HeadlessChatConfig::conversationModel();
-        $participation = HeadlessChatConfig::participationModel();
-        $message = HeadlessChatConfig::messageModel();
-        $readReceipt = HeadlessChatConfig::readReceiptModel();
-
-        Schema::create($conversation->getTable(), function (Blueprint $table) {
+        Schema::create('conversations', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('type');
@@ -22,68 +16,46 @@ return new class extends Migration
             $table->softDeletes();
         });
 
-        Schema::create($participation->getTable(), function (Blueprint $table) use ($participation, $conversation) {
+        Schema::create('participations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId($participation->conversation()->getForeignKeyName())
-                ->constrained(
-                    table: $conversation->getTable(),
-                    column: $conversation->getKeyName()
-                );
-            $table->morphs($participation->participant()->getRelationName());
+            $table->foreignId('conversation_id')
+                ->constrained(table: 'conversations', column: 'id');
+            $table->morphs('participant');
             $table->json('metadata')->default(json_encode([]));
             $table->timestamps();
 
-            $table->unique([
-                $participation->conversation()->getForeignKeyName(),
-                $participation->participant()->getMorphType(),
-                $participation->participant()->getForeignKeyName(),
-            ], 'uq_participation');
+            $table->unique(['conversation_id', 'participant_type', 'participant_id'], 'uq_participation');
         });
 
-        Schema::create($message->getTable(), function (Blueprint $table) use ($message, $participation, $conversation) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId($message->participation()->getForeignKeyName())
-                ->constrained(
-                    table: $participation->getTable(),
-                    column: $participation->getKeyName(),
-                );
-            $table->foreignId($message->conversation()->getForeignKeyName())
-                ->constrained(
-                    table: $conversation->getTable(),
-                    column: $conversation->getKeyName(),
-                );
+            $table->foreignId('participation_id')
+                ->constrained(table: 'participations', column: 'id');
+            $table->foreignId('conversation_id')
+                ->constrained(table: 'conversations', column: 'id');
             $table->text('content');
             $table->json('metadata')->default(json_encode([]));
             $table->timestamps();
             $table->softDeletes();
         });
 
-        Schema::create($readReceipt->getTable(), function (Blueprint $table) use ($readReceipt, $message, $conversation) {
+        Schema::create('read_receipts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId($readReceipt->message()->getForeignKeyName())
-                ->constrained(
-                    table: $message->getTable(),
-                    column: $message->getKeyName()
-                );
-            $table->foreignId($readReceipt->participation()->getForeignKeyName())
-                ->constrained(
-                    table: $conversation,
-                    column: $conversation->getKeyName()
-                );
+            $table->foreignId('message_id')
+                ->constrained(table: 'messages', column: 'id');
+            $table->foreignId('participation_id')
+                ->constrained(table: 'participations', column: 'id');
             $table->timestamps();
 
-            $table->unique([
-                $readReceipt->message()->getForeignKeyName(),
-                $readReceipt->participation()->getForeignKeyName(),
-            ], 'uq_read_receipt');
+            $table->unique(['message_id', 'participation_id'], 'uq_read_receipt');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists(HeadlessChatConfig::readReceiptModel()->getTable());
-        Schema::dropIfExists(HeadlessChatConfig::messageModel()->getTable());
-        Schema::dropIfExists(HeadlessChatConfig::participationModel()->getTable());
-        Schema::dropIfExists(HeadlessChatConfig::conversationModel()->getTable());
+        Schema::dropIfExists('messages');
+        Schema::dropIfExists('participations');
+        Schema::dropIfExists('conversations');
+        Schema::dropIfExists('read_receipts');
     }
 };
