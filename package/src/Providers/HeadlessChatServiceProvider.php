55f9a10bbf9a5a06744033df1d239c2ca77d7a1b
tmsperera <tmsperera@gmail.com>
2025-03-22 17:35:31 +0530

    Updated migration name

--- package/src/Providers/HeadlessChatServiceProvider.php
+++ package/src/Providers/HeadlessChatServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace TMSPerera\HeadlessChat\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class HeadlessChatServiceProvider extends ServiceProvider
@@ -13,10 +14,27 @@ class HeadlessChatServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->publishMigrations();
+        $this->publishConfig();
+    }
+
+    protected function publishMigrations(): void
+    {
+        $configPath = 'database.migrations.update_date_on_publish';
+
+        $originalConfigValue = Config::get($configPath);
+
+        Config::set($configPath, true);
+
         $this->publishesMigrations([
             __DIR__.'/../../database/migrations' => database_path('migrations'),
         ], 'headless-chat-migrations');
 
+        Config::set($configPath, $originalConfigValue);
+    }
+
+    protected function publishConfig(): void
+    {
         $this->publishes([
             __DIR__.'/../../config/headless-chat.php' => config_path('headless-chat.php'),
         ], 'headless-chat-config');
