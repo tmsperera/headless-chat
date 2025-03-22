203b223c8d6d6428986a0c66d2f8e9a4234d3191
tmsperera <tmsperera@gmail.com>
2025-03-23 01:14:14 +0530

    Revert "Removed config"
    This reverts commit 27454f29d0b2edcb79dde85acdd08201b4e9ed97.

--- package/src/Providers/HeadlessChatServiceProvider.php
+++ package/src/Providers/HeadlessChatServiceProvider.php
@@ -6,10 +6,28 @@ use Illuminate\Support\ServiceProvider;
 
 class HeadlessChatServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/headless-chat.php', 'headless-chat');
+    }
+
     public function boot(): void
+    {
+        $this->publishMigrations();
+        $this->publishConfig();
+    }
+
+    protected function publishMigrations(): void
     {
         $this->publishesMigrations([
             __DIR__.'/../../database/migrations' => database_path('migrations'),
-        ], 'headless-chat');
+        ], 'headless-chat-migrations');
+    }
+
+    protected function publishConfig(): void
+    {
+        $this->publishes([
+            __DIR__.'/../../config/headless-chat.php' => config_path('headless-chat.php'),
+        ], 'headless-chat-config');
     }
 }
