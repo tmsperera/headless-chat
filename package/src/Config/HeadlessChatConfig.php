0e02451abcb7a212c75b10a1a6a909e125ca34ed
tmsperera <tmsperera@gmail.com>
2025-03-23 01:23:29 +0530

    HeadlessChatConfig.php

--- package/src/Config/HeadlessChatConfig.php
+++ package/src/Config/HeadlessChatConfig.php
@@ -3,6 +3,7 @@
 namespace TMSPerera\HeadlessChat\Config;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 use TMSPerera\HeadlessChat\Models\Conversation;
 use TMSPerera\HeadlessChat\Models\Message;
 use TMSPerera\HeadlessChat\Models\Participation;
@@ -10,23 +11,55 @@ use TMSPerera\HeadlessChat\Models\ReadReceipt;
 
 class HeadlessChatConfig
 {
+    /**
+     * @return class-string<Conversation>
+     */
+    protected static function conversationModelClass(): string
+    {
+        return Config::get('headless-chat.models.conversation', Conversation::class);
+    }
+
+    /**
+     * @return class-string<Participation>
+     */
+    protected static function participationModelClass(): string
+    {
+        return Config::get('headless-chat.models.participation', Participation::class);
+    }
+
+    /**
+     * @return class-string<Message>
+     */
+    protected static function messageModelClass(): string
+    {
+        return Config::get('headless-chat.models.message', Message::class);
+    }
+
+    /**
+     * @return class-string<ReadReceipt>
+     */
+    protected static function readReceiptModelClass(): string
+    {
+        return Config::get('headless-chat.models.read_receipt', ReadReceipt::class);
+    }
+
     public static function conversationInstance(): Conversation
     {
-        return App::make(Conversation::class);
+        return App::make(static::conversationModelClass());
     }
 
     public static function participationInstance(): Participation
     {
-        return App::make(Participation::class);
+        return App::make(static::participationModelClass());
     }
 
     public static function messageInstance(): Message
     {
-        return App::make(Message::class);
+        return App::make(static::messageModelClass());
     }
 
     public static function readReceiptInstance(): ReadReceipt
     {
-        return App::make(ReadReceipt::class);
+        return App::make(static::readReceiptModelClass());
     }
 }
